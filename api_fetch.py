8e43846fbcb5a8231f777d5896f894750d94d271
Dillon Christensen <j.dillon.christensen@gmail.com>
2017-12-06 20:24:25 -0600

    implemented search instead of dummy example

--- api_fetch.py
+++ api_fetch.py
@@ -1,7 +1,23 @@
-from urllib.request import urlopen
+import re
+import urllib.parse
+import urllib.request
 
-def get_html_for_search(search):
-    response = urlopen('https://www.abebooks.com/servlet/SearchResults?bx=off&ds=50&recentlyadded=all&sortby=17&sts=t&an=derleth&pn=arkham')
+is_hardback = lambda x: re.compile('hardcover|hardback|^hb$|^hc$|^h$', re.IGNORECASE).match(x) is not None
+is_paperback = lambda x: re.compile('softcover|paperback|^pb$|^p$|^s$', re.IGNORECASE).match(x) is not None
+
+
+def get_html_for_search(author, title='', publisher='', year=2017, format=False):
+    querystring = {
+        'an': author,
+        'pn': publisher,
+        'tn': title,
+        'yrh': year if 999 < year < 10000 else '',
+        'bi': 0 if not format else 'h' if is_hardback(format) else 's' if is_paperback(format) else 0
+    }
+    url = 'https://www.abebooks.com/servlet/SearchResults?bx=off&ds=50&recentlyadded=all&sortby=17&sts=t&' + \
+          urllib.parse.urlencode(querystring)
+
+    response = urllib.request.urlopen(url)
     html = response.read()
     response.close()
     return str(html)
--- bookmancy.py
+++ bookmancy.py
@@ -1,9 +1,21 @@
 import api_fetch
 import api_parser
 
-def search(opts):
-    return api_parser.parse_html(api_fetch.get_html_for_search(opts))
+
+def search(author, title='', publisher='', year=2017, format=False):
+    return api_parser.parse_html(api_fetch.get_html_for_search(
+        author,
+        title,
+        publisher,
+        year,
+        format
+    ))
 
 
 if __name__ == '__main__':
-    search('hey')
+    search(
+        author='Truman Capote',
+        publisher='Random House',
+        title='In Cold Blood',
+        year=1965
+    )
